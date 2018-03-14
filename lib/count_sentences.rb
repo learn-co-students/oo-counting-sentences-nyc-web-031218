747e5385969526ed17ba84200faca8e5fb486427
Eric Kim <hyeokjungkim@gmail.com>
2018-03-14 10:59:55 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
-require 'pry'
 
 class String
 
   def sentence?
-    
+    return self[-1] == "."
   end
 
   def question?
-
+    return self[-1] == "?"
   end
 
   def exclamation?
-
+    return self[-1] == "!"
   end
 
   def count_sentences
-
+    str = self.split(/[.!?]/)
+    str.select! do |substr|
+      !substr.empty?
+    end
+    return str.length
   end
 end
