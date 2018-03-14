f2eb9f6ec01b280a0a01e78ba79fcce445382fa7
Manuel Grullon <astermanuelg@gmail.com>
2018-03-14 18:00:58 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    subsentences = []
+    subsentence = ""
+    self.each_char do |chr|
+      if [".", "!", "?"].include?(chr)
+        #do something
+        subsentences << subsentence
+        subsentence = ""
+      else
+        subsentence << chr
+      end
+    end
+    subsentences.select! do |subsentence|
+      subsentence.length > 1
+    end
+    subsentences.length
   end
 end
