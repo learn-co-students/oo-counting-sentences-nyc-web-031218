563ef5c26d70509a2e4cebc0f66c868aa869b579
KingHenryJr <henryyihengzhang@gmail.com>
2018-03-15 16:55:54 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    arr = self.split(/(?<=[?.!])/)
+    no_repeats = arr.select { |elements|
+      elements.length > 1
+    }
+    return no_repeats.count
   end
 end
