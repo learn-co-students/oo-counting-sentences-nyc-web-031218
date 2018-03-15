1b082a40ad0f6292e88e993f222bd792b08e8871
Roger Perez <roger.ucsb@gmail.com>
2018-03-14 23:50:09 -0400

    Found a shorter solution to    def count_sentences
    #it places all the punctuations we are looking for and if they ar followed
        #by a space it will split it at that point.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -18,3 +18,10 @@ class String
     self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
+# example 2
+def count_sentences
+    self.split(/[.!?] /).length #using regex
+    #it places all the punctuations we are looking for and if they ar followed
+    #by a space it will split it at that point.
+    # binding.pry
+  end
